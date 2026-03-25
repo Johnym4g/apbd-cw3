324977dbb108036f9b9ec6da83ed9bee3f00ae32
Jan Głogowski <jan.glogowski@onet.pl>
2026-03-25 21:39:13 +0100

    add: Equipment model

---
+++ MyProject/Models/Equipment.cs
@@ -0,0 +1,61 @@
+﻿namespace MyProject.Models;
+
+public abstract class Equipment
+{
+    public Guid Id { get; protected set; }
+    public string Name { get; protected set; }
+    public bool IsAvailable { get; private set; }
+
+    protected Equipment(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        IsAvailable = true;
+    }
+
+    public void MarkAsUnavailable()
+    {
+        IsAvailable = false;
+    }
+
+    public void MarkAsAvailable()
+    {
+        IsAvailable = true;
+    }
+}
+
+public class Laptop : Equipment
+{
+    public string CpuModel { get; private set; }
+    public int RamGb { get; private set; }
+
+    public Laptop(string name, string cpuModel, int ramGb) : base(name)
+    {
+        CpuModel = cpuModel;
+        RamGb = ramGb;
+    }
+}
+
+public class Projector : Equipment
+{
+    public string Resolution { get; private set; }
+    public int Lumens { get; private set; }
+
+    public Projector(string name, string resolution, int lumens) : base(name)
+    {
+        Resolution = resolution;
+        Lumens = lumens;
+    }
+}
+
+public class Camera : Equipment
+{
+    public string LensType { get; private set; }
+    public double Megapixels { get; private set; }
+
+    public Camera(string name, string lensType, double megapixels) : base(name)
+    {
+        LensType = lensType;
+        Megapixels = megapixels;
+    }
+}
