6dbccfa095cc76fd0ad6c2e47d98903ece959ca0
Jan Głogowski <jan.glogowski@onet.pl>
2026-03-25 22:38:09 +0100

    add: Result class

---
+++ MyProject/Services/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace MyProject.Services;
+public class Result
+{
+    public bool IsSuccess { get; }
+    public string ErrorMessage { get; }
+
+    private Result(bool isSuccess, string errorMessage)
+    {
+        IsSuccess = isSuccess;
+        ErrorMessage = errorMessage;
+    }
+
+    public static Result Success() => new Result(true, string.Empty);
+    
+    public static Result Fail(string message) => 
+        new Result(false, string.IsNullOrWhiteSpace(message) ? "Wystąpił nieznany błąd." : message);
+}
