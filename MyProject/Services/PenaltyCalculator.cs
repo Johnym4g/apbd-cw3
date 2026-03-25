91ae40bbed8765d98d4d1fd033d373c4449b2a2b
Jan Głogowski <jan.glogowski@onet.pl>
2026-03-25 22:31:59 +0100

    add: PenaltyCalculator class

---
+++ MyProject/Services/PenaltyCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace MyProject.Services;
+
+public class PenaltyCalculator : IPenaltyCalculator
+{
+    private readonly decimal _dailyPenaltyRate = 10.0m;
+
+    public decimal CalculatePenalty(DateTime expectedReturnDate, DateTime actualReturnDate)
+    {
+        if (actualReturnDate <= expectedReturnDate) 
+            return 0m;
+
+        var lateDays = (actualReturnDate - expectedReturnDate).Days;
+        return lateDays > 0 ? lateDays * _dailyPenaltyRate : 0m;
+    }
+}
